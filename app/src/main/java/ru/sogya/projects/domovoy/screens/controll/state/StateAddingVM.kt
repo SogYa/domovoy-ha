b641ea2774db5613e1433cecf180c1d0c4981498
SogYa <Ghost.Spartan36@gmail.com>
2023-08-11 20:03:58 +0300

    reworked

--- app/src/main/java/ru/sogya/projects/domovoy/screens/controll/state/StateAddingVM.kt
+++ app/src/main/java/ru/sogya/projects/domovoy/screens/controll/state/StateAddingVM.kt
@@ -1,4 +1,4 @@
-package ru.sogya.projects.domovoy.screens.home.bottomsheet.stateadding
+package ru.sogya.projects.domovoy.screens.controll.state
 
 import android.util.Log
 import androidx.lifecycle.LiveData
@@ -6,15 +6,11 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sogya.domain.models.StateDomain
-import com.sogya.domain.usecases.databaseusecase.states.InsertStateUseCase
 import com.sogya.domain.usecases.network.GetStatesUseCase
 import com.sogya.domain.usecases.sharedpreferences.GetStringPrefsUseCase
 import com.sogya.domain.utils.Constants
-import com.sogya.domain.utils.MyCallBack
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.launch
@@ -24,8 +20,7 @@ import javax.inject.Inject
 @HiltViewModel
 class StateAddingVM @Inject constructor(
     getStatesUseCase: GetStatesUseCase,
-    private val getStringPrefsUseCase: GetStringPrefsUseCase,
-    private val insertStateUseCase: InsertStateUseCase,
+    getStringPrefsUseCase: GetStringPrefsUseCase,
 ) : ViewModel() {
     private val statesLiveData = MutableLiveData<List<StateDomain>>()
     private val loadingViewLiveData = MutableLiveData<Int>()
@@ -42,28 +37,6 @@ class StateAddingVM @Inject constructor(
                     statesLiveData.postValue(it)
                     loadingViewLiveData.postValue(VisibilityStates.GONE.visibility)
                 }
-        }
-    }
-
-    fun addStatesToDataBase(
-        states: HashSet<StateDomain>,
-        groupId: Int,
-        myCallBack: MyCallBack<Boolean>
-    ) {
-        val ownerId = getStringPrefsUseCase.invoke(Constants.SERVER_URI)
-        val listOfStates: MutableList<StateDomain> = states.toMutableList()
-        Log.d("SetVM", listOfStates.toString())
-        if (listOfStates.isEmpty()) {
-            myCallBack.error()
-        } else {
-            listOfStates.forEach {
-                it.ownerId = ownerId
-                it.groupId = groupId
-            }
-            GlobalScope.async(Dispatchers.IO) {
-                Log.d("Start", "Start")
-                insertStateUseCase.invoke(listOfStates)
-            }
         }
     }
 
