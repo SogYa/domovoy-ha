9057fcc6d57888a6ad0ec81d23e0c5d4b8a1661e
SogYa <Ghost.Spartan36@gmail.com>
2023-08-11 19:57:31 +0300

    deleted useless constants

--- domain/src/main/java/com/sogya/domain/utils/Constants.kt
+++ domain/src/main/java/com/sogya/domain/utils/Constants.kt
@@ -10,13 +10,8 @@ object Constants {
     const val SERVER_NAME = "name"
     const val DEFAULT_GROUP_ID = -1
     const val GROUP_ID = "groupId"
-    const val IS_FIREBASE_AUTH = "firebaseAuth"
-    const val TICKET_ID_KEY = "ticketId"
     const val APP_VERSION = "0.5"
     const val STATE_ID = "stateId"
     const val INTEGRATION_SECRET = "integrationSecret"
     const val INTEGRATION_WEB_HOOK = "integrationWebHook"
-    const val NOTIFICATION_TITLE = "notification_title"
-    const val NOTIFICATION_MESSAGE = "notification_message"
-    const val CLOUD_MESSAGING_INFO_TOPIC = "info"
 }
