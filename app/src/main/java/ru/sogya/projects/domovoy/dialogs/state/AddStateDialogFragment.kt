366cba823454743bd2acd67edfa3b963b8272280
SogYa <Ghost.Spartan36@gmail.com>
2023-08-11 19:56:55 +0300

    added state controll dialog

---
+++ app/src/main/java/ru/sogya/projects/domovoy/dialogs/state/AddStateDialogFragment.kt
@@ -0,0 +1,90 @@
+package ru.sogya.projects.domovoy.dialogs.state
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.Window
+import android.view.WindowManager
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.Button
+import android.widget.Toast
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.textfield.MaterialAutoCompleteTextView
+import com.sogya.domain.models.StateDomain
+import dagger.hilt.android.AndroidEntryPoint
+import ru.sogya.projects.domovoy.R
+import ru.sogya.projects.domovoy.screens.controll.state.StateAdapter
+
+
+@AndroidEntryPoint
+class AddStateDialogFragment(private val dialogFragmentListener: DialogFragmentListener) :
+    DialogFragment() {
+    private val vm: AddStateDialogVM by viewModels()
+    private var groupdDropDownList: MaterialAutoCompleteTextView? = null
+
+    interface DialogFragmentListener {
+        fun getStateHasSet(): HashSet<StateDomain>
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.dialog_add_state, container, false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val window: Window? = dialog?.window
+        if (window != null) {
+            val lp = WindowManager.LayoutParams()
+            lp.copyFrom(window.attributes)
+            lp.width = WindowManager.LayoutParams.MATCH_PARENT
+            lp.height = WindowManager.LayoutParams.MATCH_PARENT
+            window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            window.attributes = lp
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        vm.getGroupsLiveData().observe(viewLifecycleOwner) {
+            groupdDropDownList?.setSimpleItems(it)
+
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val positiveButton: Button = view.findViewById(R.id.positive)
+        val negativeButton: Button = view.findViewById(R.id.negative)
+        groupdDropDownList =
+            view.findViewById(R.id.groupDropDownList)
+        val addStateRecyclerView: RecyclerView = view.findViewById(R.id.stateAddingDialogRecycler)
+        val stateHashSet = dialogFragmentListener.getStateHasSet()
+        val adapter = StateAdapter()
+        addStateRecyclerView.adapter = adapter
+        addStateRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        adapter.updateStatesList(stateHashSet.toList())
+        groupdDropDownList?.onItemClickListener =
+            OnItemClickListener { _, _, id, _ -> vm.setSelectedGroup(id) }
+        positiveButton.setOnClickListener {
+            vm.addStatesToDataBase(stateHashSet) {
+                if (it) {
+                    Toast.makeText(context, "Устройства добавлены", Toast.LENGTH_SHORT).show()
+                    dismiss()
+                } else {
+                    Toast.makeText(context, "Группа не выбрана", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+        negativeButton.setOnClickListener {
+            dismiss()
+        }
+    }
+}
---
+++ app/src/main/java/ru/sogya/projects/domovoy/dialogs/state/AddStateDialogVM.kt
@@ -0,0 +1,77 @@
+package ru.sogya.projects.domovoy.dialogs.state
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sogya.domain.models.StateDomain
+import com.sogya.domain.usecases.databaseusecase.groups.GetAllGroupByOwnerUseCase
+import com.sogya.domain.usecases.databaseusecase.states.InsertStateUseCase
+import com.sogya.domain.usecases.sharedpreferences.GetStringPrefsUseCase
+import com.sogya.domain.utils.Constants
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class AddStateDialogVM @Inject constructor(
+    getStringPrefsUseCase: GetStringPrefsUseCase,
+    private val getAllGroupByOwnerUseCase: GetAllGroupByOwnerUseCase,
+    private val insertStateUseCase: InsertStateUseCase
+) : ViewModel() {
+    private var groupsLiveData: MutableLiveData<Array<String?>> = MutableLiveData()
+    private var ownerId: String? = null
+    private var groupIdList:IntArray?= null
+    private var groupId:Int? = null
+
+    init {
+        ownerId = getStringPrefsUseCase.invoke(Constants.SERVER_URI)
+        getAlGroupsByOwner()
+    }
+
+    fun addStatesToDataBase(
+        states: HashSet<StateDomain>,
+        listener: ((success:Boolean) -> Unit)? = null
+    ) {
+        if (groupId != null) {
+            states.forEach {
+                it.ownerId = ownerId.toString()
+                it.groupId = groupId as Int
+            }
+            viewModelScope.launch(Dispatchers.IO) {
+                insertStateUseCase.invoke(states.toList())
+            }
+            listener?.invoke(true)
+        }else{
+            listener?.invoke(false)
+        }
+    }
+
+    private fun getAlGroupsByOwner() {
+        viewModelScope.launch {
+            ownerId?.let { s ->
+                getAllGroupByOwnerUseCase(ownerId = s).flowOn(Dispatchers.IO)
+                    .collect { stateGroupDomains ->
+                        val groupTagList = arrayOfNulls<String>(
+                            stateGroupDomains.size
+                        )
+                        groupIdList = IntArray(size = stateGroupDomains.size)
+                        var counter = 0
+                        stateGroupDomains.forEach {
+                            groupTagList[counter] = it.groupTag
+                            groupIdList!![counter] = it.groupId
+                            counter++
+                        }
+                        groupsLiveData.postValue(groupTagList)
+                    }
+            }
+        }
+    }
+
+    fun getGroupsLiveData(): LiveData<Array<String?>> = groupsLiveData
+    fun setSelectedGroup(id: Int) {
+        groupId = groupIdList?.get(id)
+    }
+}
