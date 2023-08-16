b2a5ea6f10cbf285f365668aef85fe5413eb8408
SogYa <Ghost.Spartan36@gmail.com>
2023-08-16 19:03:52 +0300

    deleted

--- data/src/main/java/com/sogya/data/mappers/state/StatesMapper.kt
+++
@@ -1,9 +0,0 @@
-package com.sogya.data.mappers.state
-
-import com.sogya.data.mappers.state.attributes.AttributeMapper
-import com.sogya.data.models.StateData
-import com.sogya.domain.models.StateDomain
-
-class StatesMapper(
-    private val stateData: StateData
-)
